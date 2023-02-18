5e5ec0078afd69616e1b352652a6d30b757fcc12
ESHAN SATYAM <eshansatyamsingh@gmail.com>
2023-02-18 18:10:23 +0530

    Print_Sum_Of_Two_Number question added

---
+++ 4.Task-1/T1_Q3_PrintSum.java
@@ -0,0 +1,14 @@
+/* Write a Java program to print the sum of two numbers. */
+
+public class T1_Q3_PrintSum {
+    public static void main(String[] args) {
+        
+        //Two integer Number
+        int a = 3;
+        int b = 4;
+
+        int c = (a+b); //Sum between two number 
+
+        System.out.println("The Sum of Number is: " + c);
+    }
+}
