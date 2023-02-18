30ec52e929d0febb40fd76480d48587a40bf5254
ESHAN SATYAM <eshansatyamsingh@gmail.com>
2023-02-18 18:31:06 +0530

    Divide_Of_Two_Number Question added

---
+++ 4.Task-1/T1_Q5_PrintDivide.java
@@ -0,0 +1,18 @@
+/* Write a Java program to divide two numbers and print on the screen. */
+
+public class T1_Q5_PrintDivide {
+
+    public static void main(String[] args) {
+        
+
+        //Two integer Number
+        int a = 14;
+        int b = 2;
+
+        //Dividing the Number
+        int c = a/b;
+
+        System.out.println("The Divide of two Number is: " + c);
+    }
+    
+}
