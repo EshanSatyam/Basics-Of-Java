b4b5ac093fa72f974e9c66964683646f8e4d59b6
ESHAN SATYAM <eshansatyamsingh@gmail.com>
2023-02-18 17:47:47 +0530

    Code modified in Right triangle Pattern question

--- 3.Challenges On Loops/pattern.java
+++ 3.Challenges On Loops/pattern.java
@@ -14,7 +14,7 @@ public class pattern {
         //FOR ROWS i FOR COLUMNS j
         for( i = 1; i<=n; i++){
 
-            for(j = 1; j<=i; j--){
+            for(j = 1; j<=i; j++){
                 System.out.print(" * ");
             }
 
