90822245e5bbe4b2caa7e43bb7f1ee2b29a1bee1
ESHAN SATYAM <eshansatyamsingh@gmail.com>
2023-02-18 17:32:45 +0530

    Right traingle Pattern using Loop question added

---
+++ 3.Challenges On Loops/pattern.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class pattern {
+    
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+        int i;
+        int j;
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+        // //FOR ROWS i FOR COLUMNS j
+        // for( i = 1; i<=n; i++){
+
+        //     for(j = 1; j<=i; j--){
+        //         System.out.print(" * ");
+        //     }
+
+        //     System.out.println("  ");
+        // }
+
+        for (i = 1; i<=n; i++){
+
+            for(j = n; j>=n; j--){
+
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+       
+        }
+    }
+
