b4fe969d85081c6151fb45905a2fa37a394d4cb1
ESHAN SATYAM <eshansatyamsingh@gmail.com>
2023-02-19 12:16:47 +0530

    Name of file changed and code corrected

--- 4.Task-1/T1_Q2_HelloWorld.java
+++ 4.Task-1/T1_Q2_HelloWorld.java
@@ -1,9 +1,9 @@
 /* Write a basic “Hello, World!” JAVA program */
 
-public class T_02_HelloWorld {
+public class T1_Q2_HelloWorld {
      
     public static void main(String[] args) {
         
-        System.out.println("Hello World");
+        System.out.println("Hello, World!");
     }
 }
